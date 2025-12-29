8957b1e6a3afdcdd71db6d7f7de3a7bc58102b9a
nurti-linux <questgar@gmail.com>
2025-12-30 01:16:32 +0500

    base functions

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from todo!")
+from datetime import date
 
+todo_list = {}
+
+def todo_add(text, description):
+    today = date.today()
+
+    todo = {
+        text: {
+            "description": description,
+            "added date": f"{today.day}.{today.month}.{today.year}",
+            "completed": False
+        }
+    }
+
+    todo_list.update(todo)
+
+def delete_todo(text):
+    if text in todo_list:
+        todo_list.pop(text)
+
+def list_todo():
+    return todo_list
+
+def main():
+    todo_add("test", "re")
+    todo_add("fys", "kys")
+    delete_todo("test")
+    todo_add("55", "suck")
+    print(list_todo())
 
 if __name__ == "__main__":
     main()
