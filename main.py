93ad1159a45350fd57c67c2abb028a9f59b0f127
nurt1- <questgar@gmail.com>
2026-01-04 15:27:21 +0500

    intrigued by working with a JSON file

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 from datetime import date
+import json
 
-todo_list = {}
+FILE_NAME = "todo.json"
+
+with open(FILE_NAME, "w") as file:
+    pass
+
+with open(FILE_NAME, "r") as file:
+    if file.read():
+        todo_list = json.load(file)
+    else:
+        todo_list = {}
+
+def todo_update():
+    with open(FILE_NAME, "w") as file:
+        json.dump(todo_list, file, indent=4)
 
 def todo_add(text, description):
     today = date.today()
 
-    todo = {
-        text: {
-            "description": description,
-            "added date": f"{today.day}.{today.month}.{today.year}",
-            "completed": False
+    if text not in todo_list:
+        todo = {
+            text: {
+                "description": description,
+                "added date": f"{today.day}.{today.month}.{today.year}",
+                "completed": False
+            }
         }
-    }
 
-    todo_list.update(todo)
+        todo_list.update(todo)
+        todo_update()
 
 def delete_todo(text):
     if text in todo_list:
         todo_list.pop(text)
+        todo_update()
 
 def list_todo():
     return todo_list
@@ -27,6 +44,7 @@ def main():
     todo_add("fys", "kys")
     delete_todo("test")
     todo_add("55", "suck")
+    todo_add("55", "nah")
     print(list_todo())
 
 if __name__ == "__main__":
